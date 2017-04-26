2b37b860486dadda5c98357662bf5a408b9ebf17
Joseph Diku <joseph.diku@gmail.com>
2017-04-26 08:18:13 +0300

    add docopt decorator function
    I use this function in the main application that requires docopt

---
+++ app_plugins/docopt_extension.py
@@ -0,0 +1,34 @@
+from docopt import docopt, DocoptExit
+
+
+def docopt_cmd(func):
+    """
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+
+    Reused Code: http://code.runnable.com/Us9xVgFEERtWAAHp/how-to-use-interactive-shell-in-docopt-for-python
+    """
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('Invalid Command!')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
