1de108356d0bdde11a4dba34b8584d1d0e10fed7
Joseph Diku <joseph.diku@gmail.com>
2017-04-26 18:31:25 +0300

    Update tests for create room
    Single argument with a dictionary which contains the values like that in docopt

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,22 +3,22 @@ from dojo_application import DojoApplication
 from app.office import Office
 from app.living_space import LivingSpace
 
+from app.my_dojo import MyDojo
+from app import my_dojo
+
 
 class TestCreateRoom(unittest.TestCase):
     """Tests for the create_room command"""
     def setUp(self):
-        self.dojo_app = DojoApplication()
+        self.test_dojo = MyDojo()
 
-    def test_create_room_office(self):
-        self.assertEqual(self.dojo_app.create_room(self, 'office', 'Blue'), Office)
+        """Create a test living space"""
+        self.test_dojo.create_room({"<room_name>": ["Black"], "livingspace": True, "office": False})
+        """Create a test office"""
+        self.test_dojo.create_room({"<room_name>": ["White"], "livingspace": False, "office": True})
 
-    def test_create_room_livingspace(self):
-        self.assertEqual(self.dojo_app.create_room(self, 'livingspace', 'Blue'), LivingSpace)
-
-    def test_create_room_multiple_office(self):
-        self.assertEqual(
-            self.dojo_app.create_room(self, 'office', 'Blue', 'Black', 'Red'), len(self.dojo_app.offices))
-
-    def test_create_room_multiple_livingspace(self):
-        self.assertEqual(
-            self.dojo_app.create_room(self, 'livingspace', 'Blue', 'Black', 'Red'), len(self.dojo_app.livingspaces))
+    def test_create_room(self):
+        # Office 'White', and Living Space room 'Black' already exist in memory from setUp function
+        self.assertEqual(1, len(self.test_dojo.offices))
+        self.assertEqual(1, len(self.test_dojo.livingspaces))
+        self.assertEqual(2, len(self.test_dojo.rooms))
