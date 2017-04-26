663c9c5e8a4d78856a8cdfbc7f9dfc3641a65f28
Joseph Diku <joseph.diku@gmail.com>
2017-04-26 20:17:23 +0300

    Update class definition for Person class

--- app/person.py
+++ app/person.py
@@ -1,2 +1,11 @@
+# Person class
+# Creates a Person object that Fellow and Staff classes inherit from
+
+
 class Person(object):
-    pass
+    """Creates a Person object that Fellow and Staff class inherit from"""
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Person %s>" % self.name
