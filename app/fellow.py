1573480bc713c3ab5daa61061b59c37a1136ecc5
Joseph Diku <joseph.diku@gmail.com>
2017-04-26 20:16:46 +0300

    Update class definition for Fellow class

--- app/fellow.py
+++ app/fellow.py
@@ -1,5 +1,14 @@
 from app.person import Person
 
+# Fellow class
+# Creates a Fellow object that inherits from Person class
+
 
 class Fellow(Person):
-    pass
+    person_type = 'Fellow'
+
+    def __init__(self, name):
+        super(Fellow, self).__init__(name)
+
+    def __repr__(self):
+        return "<Fellow %s>" % self.name
