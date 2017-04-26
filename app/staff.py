3b5e98f35ff827ac574ea0437a0a9666cd5f2bfc
Joseph Diku <joseph.diku@gmail.com>
2017-04-26 20:18:31 +0300

    Update Staff class definition

--- app/staff.py
+++ app/staff.py
@@ -1,5 +1,14 @@
 from app.person import Person
 
+# Fellow class
+# Creates a Staff object that inherits from Person class
+
 
 class Staff(Person):
-    pass
+    person_type = 'Staff'
+
+    def __init__(self, name):
+        super(Staff, self).__init__(name)
+
+    def __repr__(self):
+        return "<Staff %s>" % self.name
