4cb80f64642b6b72f031bef31e7c701ab8dba5ff
Joseph Diku <joseph.diku@gmail.com>
2017-04-26 08:18:52 +0300

    add Room class

--- app/room.py
+++ app/room.py
@@ -1,2 +1,15 @@
+from app.person import Person
+from app.dojo import Dojo
+from abc import ABCMeta, abstractmethod
+
+
 class Room(object):
-    pass
+    def __init__(self):
+        self.person = Person()
+        self.dojo = Dojo()
+        self.rooms = []
+
+    @abstractmethod
+    def get_room_availability(self):
+        """Return the availability of the room"""
+        pass
